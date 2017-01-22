9023d8dbea248de2a9747710a74014929838b09c
bchen03 <bchen03@yahoo.com>
2017-01-21 22:08:47 -0500

    Fixed namespace and class missing issue.

--- helloworld.cs
+++ helloworld.cs
@@ -1,7 +1,12 @@
-using System;
 using System.IO;
 
-public static void Main(string[] args)
+namespace mPlatform
 {
-    Console.WriteLine("Hello World");
+    public class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
 }
